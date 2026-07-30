67813703185229b58c1cf7ea0dd02d1f8084697a
FatimaNoorAhmed <fatimamansoor.fm30@gmail.com>
2026-07-31 01:54:08 +0500

    Update

--- Client/src/components/ExpenseForm.jsx
+++ Client/src/components/ExpenseForm.jsx
@@ -18,7 +18,7 @@ function ExpenseForm({ fetchExpenses }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    await API.post("/api/expenses", formData);
+    await API.post("/expenses", formData);
 
     setFormData({
       title: "",
